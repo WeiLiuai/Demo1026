3d08018845681b87aa8de8f7b3e1d07da386143a
孑遗 <18203512650@163.com>
2020-10-29 12:15:52 +0800

    test branch

--- src/test/java/com/example/demo1026/Demo1026ApplicationTests.java
+++ src/test/java/com/example/demo1026/Demo1026ApplicationTests.java
@@ -16,7 +16,7 @@ class Demo1026ApplicationTests {
 
 		System.out.println("-------------------Period Part1----------------------------");
 
-		LocalDate startDate = LocalDate.of(1988,5,8);
+		LocalDate startDate = LocalDate.of(1988,6,8);
 		LocalDate endDate = LocalDate.now();
 
 		//计算日期的差值
