f1feb8b23869efd981ba1f670576fd42c405db8d
WeiLiuai <18203512650@163.com>
2020-10-30 17:17:21 +0800

    testBranch增加一行代码

--- src/test/java/com/example/demo1026/Demo1026ApplicationTests.java
+++ src/test/java/com/example/demo1026/Demo1026ApplicationTests.java
@@ -89,6 +89,7 @@ class Demo1026ApplicationTests {
 	//这是分支合并测试方法
 	@Test
 	public void merge(){
+		int a = 20;
 		System.out.println("这是分支合并测试方法");
 		System.out.println("这是testBranch方法新增列");
 		System.out.println("testBranch 17.01");
